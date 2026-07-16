9eb8a17a29757b4f923482c1fedd2056c0932528
Yash Nagve Patil <yashnagve5@gmail.com>
2026-07-16 14:52:44 +0530

    multer setup done

--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import { authenticateSeller } from '../middlewares/auth.middleware.js';
 import { createProduct } from '../controllers/product.controller.js';
+import multer from "multer"
+
+const upload = multer({
+    storage:multer.memoryStorage(),
+    limits:{
+        fileSize:5 * 1024 * 1024// 5 mb
+    }
+})
+
 
 const router = express.Router();
 
 
-router.post("/",authenticateSeller,createProduct)
+router.post("/",authenticateSeller,upload.array('images',7),createProduct)
 
 
 
