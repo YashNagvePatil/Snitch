268324a646416b38fea3aad1e3446fe880fa0052
Yash Nagve Patil <yashnagve5@gmail.com>
2026-07-16 15:07:03 +0530

    product controller setup done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,8 +1,32 @@
 import productModel from '../models/product.model'
-
+import { uploadFile } from '../services/storage.service.js';
 
 export async function createProduct (req,res) {
  
-    
+    const {title,description,price,} = req.body;
+    const seller = req.user;
 
+    const images = await Promise.all(req.files.map(async(file) =>{
+          return await uploadFile({
+            buffer:file.buffer,
+            fileName:file.orginalname
+          })
+    }))
+
+    const product = await productModel.create({
+                title,
+                description,
+                price:{
+                    amount:price,
+                    currency:"INR"
+                },
+                images,
+                seller:seller._id
+    })
+
+    res.status(201).json({
+        message:"Product created successfully",
+        success:true,
+        product
+    })
 }
