8201bac88dc52ecaec8d688c559a831d6ef13457
Yash Nagve Patil <yashnagve5@gmail.com>
2026-07-16 12:03:26 +0530

    products schema done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,11 +9,13 @@ const userSchema = new mongoose.Schema({
      },
 
      contact:{
-      type:String,required:true
+      type:String,required:false
      },
      password:{
         type:String,
-        required:true
+        required: function() {
+           return !this.googleId;
+        }
      },
      role:{
         type:String,
@@ -23,6 +25,9 @@ const userSchema = new mongoose.Schema({
      fullName:{ 
          type:String,
         required:true 
+     },
+     googleId:{
+      type:String
      }
 
 })
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { validateRegisteruser,validateLoginUser } from "../validators/auth.validator.js";
-import { login, register } from "../controllers/auth.controller.js";
+import { googleCallback, login, register } from "../controllers/auth.controller.js";
+import passport from "passport";
+import { config } from "../config/config.js";
 const router = Router()
 
 router.post("/register", validateRegisteruser,register) 
 
 router.post("/login",validateLoginUser,login)
  
+router.get("/google",
+    passport.authenticate("google",{scope:["profile","email"]}))
 
+router.get('/google/callback',
+    passport.authenticate('google',
+    {session:false,
+    failureRedirect:process.env.NODE_ENV == "development" ?"http://localhost:5173/login" : "/login"}),
+    googleCallback
+)
 export default router;
