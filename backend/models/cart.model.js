d349a62c270ddc51980e924899fb9fada5186967
Yash Nagve Patil <yashnagve5@gmail.com>
2026-07-25 05:15:35 +0530

    cart model and price schema created

---
+++ backend/models/cart.model.js
@@ -0,0 +1,35 @@
+import mongoose, { trusted } from "mongoose"
+import priceSchema from "./price.schema"
+
+
+const cartItemSchema = new mongoose.Schema({
+         user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'user',
+            required:true
+         },
+
+         itmes:[
+            {
+                product:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:'product',
+                    required:true
+                },
+
+                variant:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:'varient'
+                },
+
+                quantity:{
+                    type:Number,
+                    default: 1 
+                },
+                price:{
+                           type:priceSchema,
+                           required:true
+                }
+            }
+         ]
+})
